ceb8bd0f9450bf47f45d72af4e77d131ebcdd822
arthurdm10 <arthurdemacq@hotmail.com>
2019-09-10 13:42:14 -0300

    Add sort process list

--- lib/pages/processes_page.dart
+++ lib/pages/processes_page.dart
@@ -11,9 +11,18 @@ class ProcessesPage extends StatefulWidget {
   _ProcessesPageState createState() => _ProcessesPageState();
 }
 
+enum SortProcsBy { NAME, CPU, RAM }
+
 class _ProcessesPageState extends State<ProcessesPage>
     with AutomaticKeepAliveClientMixin {
   WebSocketProvider _ws;
+  SortProcsBy _sortBy = SortProcsBy.NAME;
+
+  static const _sortKey = {
+    SortProcsBy.NAME: "name",
+    SortProcsBy.CPU: "cpu_percent",
+    SortProcsBy.RAM: "memory_percent",
+  };
 
   @override
   void initState() {
@@ -24,8 +33,13 @@ class _ProcessesPageState extends State<ProcessesPage>
   }
 
   @override
-  // TODO: implement wantKeepAlive
   bool get wantKeepAlive => true;
+
+  _setSortBy(SortProcsBy sortBy) {
+    setState(() {
+      _sortBy = sortBy;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,12 +60,20 @@ class _ProcessesPageState extends State<ProcessesPage>
               final data = cmdResponse.data["response"];
               final memData = data["memory"];
               final List procs = data["processes"];
-              procs.sort(
-                  (p1, p2) => p2["memory_percent"].compareTo(p1["memory_percent"]));
+              procs.sort((p1, p2) {
+                final sortKey = _sortKey[_sortBy];
+                if (_sortBy == SortProcsBy.NAME) {
+                  return p1[sortKey]
+                      .toLowerCase()
+                      .compareTo(p2[sortKey].toLowerCase());
+                }
+
+                return p2[sortKey].compareTo(p1[sortKey]);
+              });
               int cpuCount = 1;
 
               return Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 8),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
@@ -61,10 +83,12 @@ class _ProcessesPageState extends State<ProcessesPage>
                     ),
                     SizedBox(height: 8),
                     GridView.count(
+                      physics: NeverScrollableScrollPhysics(),
                       crossAxisCount: 3,
                       shrinkWrap: true,
                       crossAxisSpacing: 20,
                       childAspectRatio: 3.5,
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
                       children: data["cpu"]["percent"].map<Widget>((cpuPercent) {
                         return Text(
                           'CPU ${cpuCount++}: $cpuPercent%',
@@ -78,10 +102,12 @@ class _ProcessesPageState extends State<ProcessesPage>
                     ),
                     SizedBox(height: 8),
                     GridView.count(
+                      physics: NeverScrollableScrollPhysics(),
                       crossAxisCount: 3,
                       shrinkWrap: true,
                       crossAxisSpacing: 20,
                       childAspectRatio: 3.5,
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
                       children: [
                         Text(
                           'Total: ${fileSizeFormated(memData["total"])}',
@@ -94,29 +120,83 @@ class _ProcessesPageState extends State<ProcessesPage>
                         ),
                       ],
                     ),
-                    SizedBox(height: 8),
+                    SizedBox(height: 12),
+                    Container(
+                      color: Color(0xff2196f3),
+                      padding:
+                          const EdgeInsets.symmetric(vertical: 8, horizontal: 3),
+                      margin: const EdgeInsets.only(bottom: 5),
+                      child: DefaultTextStyle(
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () {
+                                  _setSortBy(SortProcsBy.NAME);
+                                },
+                                child: Text("NAME"),
+                              ),
+                            ),
+                            Flexible(
+                              fit: FlexFit.tight,
+                              child: GestureDetector(
+                                onTap: () {
+                                  _setSortBy(SortProcsBy.CPU);
+                                },
+                                child: Text("CPU"),
+                              ),
+                            ),
+                            Flexible(
+                              fit: FlexFit.loose,
+                              child: GestureDetector(
+                                onTap: () {
+                                  _setSortBy(SortProcsBy.RAM);
+                                },
+                                child: Text("RAM"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                     Expanded(
                       child: ListView(
                         shrinkWrap: true,
                         primary: true,
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        itemExtent: 20,
                         children: procs.map((proc) {
-                          return Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(child: Text(proc["name"])),
-                              Flexible(
-                                fit: FlexFit.tight,
-                                child: Text(
-                                  proc["cpu_percent"].toStringAsFixed(2),
+                          return Dismissible(
+                            key: UniqueKey(),
+                            onDismissed: (_) {},
+                            confirmDismiss: (_) {
+                              return Future.delayed(
+                                  Duration(seconds: 2), () => true);
+                            },
+                            dismissThresholds: {DismissDirection.endToStart: 0.2},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(child: Text(proc["name"])),
+                                Flexible(
+                                  fit: FlexFit.tight,
+                                  child: Text(
+                                    '${proc["cpu_percent"].toStringAsFixed(2)}%',
+                                  ),
                                 ),
-                              ),
-                              Flexible(
-                                fit: FlexFit.loose,
-                                child: Text(
-                                  proc["memory_percent"].toStringAsFixed(2),
+                                Flexible(
+                                  fit: FlexFit.loose,
+                                  child: Text(
+                                    '${proc["memory_percent"].toStringAsFixed(2)}%',
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           );
                         }).toList(),
                       ),
