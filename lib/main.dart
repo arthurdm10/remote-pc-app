5e3b6ca2777b4df8150be9346eea2d5989978794
arthurdm10 <arthurdemacq@hotmail.com>
2019-09-10 13:42:35 -0300

    set root widget as stateful

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,25 @@ import 'package:remote_pc/providers/websocket_provider.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   final pcKey = "fc58161e6b0da8e0cae8248f40141165";
+
+  WebSocketProvider ws;
+
+  @override
+  void initState() {
+    ws = WebSocketProvider("10.0.3.2", 9002, pcKey);
+    // final ws = WebSocketProvider("192.168.0.110", 9002, pcKey);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final ws = WebSocketProvider("10.0.3.2", 9002, pcKey);
-    // final ws = WebSocketProvider("192.168.0.110", 9002, pcKey);
-
     return MaterialApp(
       title: 'Material App',
       home: MultiProvider(
